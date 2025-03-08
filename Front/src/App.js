b11a9f8224e94820c9697e38f7747126048bcf4c
Sachintha Chathuranga <smschathu1234@gmail.com>
2025-03-09 04:31:29 +0530

    Create Dashboard

---
+++ Front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NetworkDashboard from './pages/NetworkDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1 className='text-red-500'>hello</h1> */}
+      <NetworkDashboard />
+    </div>
+  );
+}
+
+export default App;
